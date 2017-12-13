c7144b6f5e7bd66af43612fcee82e7a56c57188d
rddtw099999 <rddtw099999@gmail.com>
2017-12-14 00:19:31 +0800

    HW6

---
+++ P10-7/source/main.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct _card{
+	char *face;
+	char *suit;
+}card;
+
+int main(void)
+{
+	card aCard;
+	card *cardPtr;
+
+	aCard.face = "Ace";
+	aCard.suit = "Spades";
+
+	cardPtr = &aCard;
+
+	printf("%s%s%s\n%s%s%s\n%s%s%s\n", aCard.face, " of ", aCard.suit, cardPtr->face, " of ", cardPtr->suit, (*cardPtr).face, " of ", (*cardPtr).suit);
+	system("pause");
+	return 0;
+}
